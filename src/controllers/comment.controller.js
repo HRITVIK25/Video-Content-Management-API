f3a7c8765990e688150dfeb79716f236365cab1c
Hritvik Tayal <hritviktayal74@gmail.com>
2024-07-13 23:19:42 +0530

    fix bugs

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,8 +1,6 @@
 import { Mongoose } from "mongoose";
-import jwt from "jsonwebtoken";
 import { Comment } from "../models/comment.model.js";
 import { User } from "../models/user.model.js";
-import { Video } from "../models/video.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -69,7 +67,9 @@ const updateComment =  asyncHandler(async(req,res)=>{
     return res
     .status(200)
     .json(new ApiResponse(200, updatedComment, "Comment updated successfully"))
-})
+});
+
+const deleteComment = asyncHandler(async(req,res)=>{});
 
 export { 
     addComment,
