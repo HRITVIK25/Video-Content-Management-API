3c97910391c61ccf30a8bd0cf26635859e8a83e8
Hritvik Tayal <hritviktayal74@gmail.com>
2024-07-13 20:56:52 +0530

    update comment router and fix bugs

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -15,11 +15,17 @@ const addComment = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Please enter content");
   }
 
+  if(content.length>300){
+    throw new ApiError(400, "Content should be less than 300 characters")
+  }
+
   if (!videoId) {
     throw new ApiError(400, "Video not found");
   }
 
   const user = await User.findById(req.user?._id);
+
+  if(!user){}
 
   const comment = await Comment.create({
     content: content,
@@ -32,4 +38,40 @@ const addComment = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, comment, "Comment created successfully"));
 });
 
-export { addComment };
+const updateComment =  asyncHandler(async(req,res)=>{
+    const { content } = req.body;
+    const { id: commentId } = req.params;
+
+    if(!commentId){
+        throw new ApiError(400, "Comment not found")
+    }
+
+    if(!content){
+        throw new ApiError(400, "No content found to update")
+
+    }
+
+    if(content.length>300){
+        throw new ApiError(400, "Content should be less than 300 characters")
+      }
+
+    const updatedComment = await Comment.findByIdAndUpdate(commentId,
+        {
+            $set: {
+                content: content
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updatedComment, "Comment updated successfully"))
+})
+
+export { 
+    addComment,
+    updateComment 
+};
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,12 +1,14 @@
-import { Router } from "express"
-import { addComment } from "../controllers/comment.controller.js"
-
+import { Router } from "express";
+import {
+  addComment,
+  updateComment,
+} from "../controllers/comment.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-router.use(verifyJWT)
+router.use(verifyJWT);
 
-router.route("/addComment/:id").post(addComment)
-
+router.route("/addComment/:id").post(addComment);
+router.route("/updateComment/:id").patch(updateComment);
 
 export default router;
