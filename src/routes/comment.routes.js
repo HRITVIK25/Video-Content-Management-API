aced630dae2d2ed9447bd70d4ca8c2fafde86399
Hritvik Tayal <hritviktayal74@gmail.com>
2024-07-14 00:03:06 +0530

    update comment route

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   addComment,
+  deleteComment,
   updateComment,
 } from "../controllers/comment.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -10,5 +11,6 @@ router.use(verifyJWT);
 
 router.route("/addComment/:id").post(addComment);
 router.route("/updateComment/:id").patch(updateComment);
+router.route("/deleteComment/:id").delete(deleteComment);
 
 export default router;
