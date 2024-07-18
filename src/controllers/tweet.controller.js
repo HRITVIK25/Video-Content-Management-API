f232f7e72a96c21d59b4807ef50b2067c03e8865
Hritvik Tayal <hritviktayal74@gmail.com>
2024-07-18 22:03:10 +0530

    update tweet controller and tweet routes

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,6 +1,42 @@
 import { Mongoose } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
 import { User } from "../models/user.model.js";
 import { Video } from "../models/video.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+
+const createTweet = asyncHandler(async (req, res) => {
+    const {content} = req.body;
+
+    if(!content){
+        throw new ApiError(400, "Content is required")
+    }
+
+    if(content.length>300){
+        throw new ApiError(400, "Only 300 characters allowed")
+    }
+
+    const user = await User?.findById(req.user?._id);
+
+    if(!user){
+        throw new ApiError(400, "user not found")
+    }
+
+    const tweet = await Tweet.create({
+        content: content,
+        owner: user?._id
+    })
+
+    if(!tweet){
+        throw new ApiError(400, "Error in creating a tweet")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet created successfully"))
+});
+
+export {
+    createTweet
+};
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,8 +1,10 @@
 import {Router} from "express";
-import {} from "../controllers/tweet.controller.js"
+import { createTweet } from "../controllers/tweet.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+router.use(verifyJWT)
 
+router.route("/createTweet").post(createTweet);
 
 export default router;
