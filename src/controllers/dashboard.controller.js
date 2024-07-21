8696d95765411c22d46454759421b650196c52ac
Hritvik Tayal <hritviktayal74@gmail.com>
2024-07-21 17:58:41 +0530

    create dashboard playlist congroller and routes

---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,12 @@
+import { Mongoose } from "mongoose";
+import {Video} from "../models/video.model.js"
+import {User} from "../models/user.model.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const getChannelLike = asyncHandler(async(req,res)=>{
+    
+})
+
+export {}
