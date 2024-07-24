f0b7ab2a3b92897df80c7228b51e32c6e5e4defb
Hritvik Tayal <hritviktayal74@gmail.com>
2024-07-24 19:03:44 +0530

    create subscribtion controller and routes update like controller

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ import healthcheckRouter from "./routes/healthcheck.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js"
 
 //routes decleration
 app.use("/api/v1/users", userRouter);
@@ -35,5 +36,6 @@ app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
 
 export { app };
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,16 +1,9 @@
 import { Mongoose } from "mongoose";
 import { User } from "../models/user.model.js";
 import { Video } from "../models/video.model.js";
-import { Comment } from "../models/comment.model.js";
-import { Tweet } from "../models/tweet.model.js";
-import { Like } from "../models/like.model.js";
+import {Subscription} from "../models/subscription.model.js"
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-
-const toggleVideoLike = asyncHandler(async(req,res)=>{
-    
-})
-
-export {};
+export {}
